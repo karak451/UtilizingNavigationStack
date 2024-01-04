8732a466a9ceee1d749069510e79a8d821579354
Istvan Sudar <pityutanarur@gmail.com>
2024-01-04 15:17:04 +0100

    Initial Commit

---
+++ UtilizingNavigationStack/UtilizingNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilizingNavigationStackApp.swift
+//  UtilizingNavigationStack
+//
+//  Created by Sudar Istvan on 2024. 01. 04..
+//
+
+import SwiftUI
+
+@main
+struct UtilizingNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
